71f8a4daddcbc1a2c20e1453c1e08494fe022059
iambrianwalker <bwalker37837@gmail.com>
2026-03-18 11:08:52 -0400

    feat: Started importing packages and connecting repository to database helper

--- lib/repositories/habit_repository.dart
+++ lib/repositories/habit_repository.dart
@@ -1,1 +1,11 @@
 import 'package:sqflite/sqflite.dart';
+import '../models/habit_model.dart';
+
+class HabitRepository {
+  final DatabaseHelper _dbHelper = DatabaseHelper.instance;
+
+  Future<int> insertHabit(Habit habit) async {
+    final db = await _dbHelper.database;
+    return db.insert('habits', habit.toMap());
+  }
+}
