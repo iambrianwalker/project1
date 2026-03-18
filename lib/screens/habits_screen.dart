68497e162eeb0631b2a0640693971f928bd61966
Alan Pruett <cpruett3@student.gsu.edu>
2026-03-17 21:07:24 -0400

    feat: add temporary fake habits for habits screen

--- lib/screens/habits_screen.dart
+++ lib/screens/habits_screen.dart
@@ -15,3 +15,57 @@ class _HabitsScreenState extends State<HabitsScreen>{
     );
   }
 }
+
+//placeholder habits data
+final habits = [
+  {
+    'id': 1,
+    'image_url' : 'imageUrl',
+    'habit_name' : 'Study Flutter',
+    'habit_description' : 'Spend at least 30 minutes a day learning Flutter and building projects to improve my skills.',
+    'category' : 'Education',
+    'frequency' : 'Daily',
+    'current_streak' : '2',
+    'total_completions' : '2',
+  },
+  {
+    'id': 2,
+    'image_url' : 'imageUrl',
+    'habit_name' : 'Study Dart',
+    'habit_description' : 'Spend at least 30 minutes a day learning Dart and building projects to improve my skills.',
+    'category' : 'Education',
+    'frequency' : 'Daily',
+    'current_streak' : '2',
+    'total_completions' : '2',
+  },
+  {
+    'id': 3,
+    'image_url' : 'imageUrl',
+    'habit_name' : 'Study SQLite',
+    'habit_description' : 'Spend at least 30 minutes a day learning SQLite and building projects to improve my skills.',
+    'category' : 'Education',
+    'frequency' : 'Daily',
+    'current_streak' : '2',
+    'total_completions' : '2',
+  },
+  {
+    'id': 4,
+    'image_url' : 'imageUrl',
+    'habit_name' : 'Exercise',
+    'habit_description' : 'Spend at least 30 minutes a day exercising to improve my physical health and well-being.',
+    'category' : 'Health',
+    'frequency' : 'Daily',
+    'current_streak' : '3',
+    'total_completions' : '2',
+  },
+  {
+    'id': 5,
+    'image_url' : 'imageUrl',
+    'habit_name' : 'Cook Healthy Meals',
+    'habit_description' : 'Cook more vegetables!',
+    'category' : 'Health',
+    'frequency' : 'Daily',
+    'current_streak' : '1',
+    'total_completions' : '2',
+  },
+];
