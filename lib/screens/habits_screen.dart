6c8416f1cbad7fe62cb38220a5b014115fb52d33
Alan Pruett <cpruett3@student.gsu.edu>
2026-03-17 21:55:20 -0400

    feat: build out search bar widget

--- lib/screens/habits_screen.dart
+++ lib/screens/habits_screen.dart
@@ -41,6 +41,32 @@ class HabitsTitle extends StatelessWidget {
 }
 
 //##TODO search bar widget for habits screen
+class HabitsSearchBar extends StatelessWidget {
+  final TextEditingController controller;
+  final ValueChanged<String> onChanged;
+
+  const HabitsSearchBar({
+    super.key,
+    required this.controller,
+    required this.onChanged,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: controller,
+      onChanged: onChanged,
+      decoration: InputDecoration(
+        hintText: 'Search habits',
+        prefixIcon: const Icon(Icons.search),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(12),
+        ),
+        filled: true,
+      ),
+    );
+  }
+}
 
 //##TODO widget to add a habit
 
