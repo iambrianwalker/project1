37340d8a1176a4f40662de0b9c09a8d01862b55b
iambrianwalker <bwalker37837@gmail.com>
2026-03-18 11:35:35 -0400

    feat: Added primary keys on habit ids just in case we need them. Creatd the _createDB function to create our table for the habits with all the categories and spaces necessary

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -6,4 +6,42 @@ class DatabaseHelper {
 
   DatabaseHelper._init();
 
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+    _database = await _initDatabase('habit_mastery.db');
+    return _database!;
+  }
+
+  Future<Database> _initDatabase(String filePath) async {
+    final dbPath = await getDatabasesPath();
+    final path = join(dbPath, filePath);
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onConfigure: _onConfigure,
+      onCreate: _createDB,
+    );
+  }
+
+  Future<void> _onConfigure(Database db) async {
+    await db.execute('PRAGMA foreign_keys = ON');
+  }
+
+  Future<void> _createDB(Database db, int version) async {
+    await db.execute(
+      '''
+      CREATE TABLE habits (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        image_url TEXT,
+        habit_name TEXT NOT NULL,
+        habit_description TEXT NOT NULL,
+        category TEXT NOT NULL,
+        frequncy TEXT NOT NULL,
+        current_streak INTEGER NOT NULL,
+        total_completions INTEGER NOT NULL
+      )
+      '''
+    );
+  }
 }
