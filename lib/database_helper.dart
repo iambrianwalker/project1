d12ef63bddf2bc4d7b5f48dded3ef498e380f764
Alan Pruett <cpruett3@student.gsu.edu>
2026-03-19 18:59:45 -0400

    fix: typo in DatabaseHelper._init()

--- lib/database_helper.dart
+++ lib/database_helper.dart
@@ -2,7 +2,7 @@ import 'package:sqflite/sqflite.dart';
 import 'package:path/path.dart';
 
 class DatabaseHelper {
-  static final DatabaseHelper instance = DatabaseHelper.init();
+  static final DatabaseHelper instance = DatabaseHelper._init();
   static Database? _database;
 
   DatabaseHelper._init();
