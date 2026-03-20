cfc54cb6c889a0c5e937b587cb9dca6f68e9c074
Alan Pruett <cpruett3@student.gsu.edu>
2026-03-19 21:25:36 -0400

    feat: create controllers for all the habit fields in edit habit screen, set up initState for them, and set up dispose for them

--- lib/screens/edit_habit_screen.dart
+++ lib/screens/edit_habit_screen.dart
@@ -1,10 +1,65 @@
 import 'package:flutter/material.dart';
+import '../models/habit_model.dart';
+import '../repositories/habit_repository.dart';
 
-class EditHabitScreen extends StatelessWidget {
+class EditHabitScreen extends StatefulWidget {
   //this can be null to allow for using as add habit or edit habit
-  final Map<String, dynamic>? habit;
+  final Habit? habit;
 
   const EditHabitScreen({super.key, this.habit});
+
+  @override
+  State<EditHabitScreen> createState() => _EditHabitScreenState();
+}
+
+class _EditHabitScreenState extends State<EditHabitScreen> {
+  final HabitRepository _habitRepository = HabitRepository();
+
+  late final TextEditingController _habitNameController;
+  late final TextEditingController _habitDescriptionController;
+  late final TextEditingController _categoryController;
+  late final TextEditingController _frequencyController;
+  late final TextEditingController _currentStreakController;
+  late final TextEditingController _totalCompletionsController;
+  late final TextEditingController _imageUrlController;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _habitNameController = TextEditingController(
+      text: widget.habit?.habitName ?? '',
+    );
+    _habitDescriptionController = TextEditingController(
+      text: widget.habit?.habitDescription ?? '',
+    );
+    _categoryController = TextEditingController(
+      text: widget.habit?.category ?? '',
+    );
+    _frequencyController = TextEditingController(
+      text: widget.habit?.frequency ?? '',
+    );
+    _currentStreakController = TextEditingController(
+      text: widget.habit?.currentStreak.toString() ?? '',
+    );
+    _totalCompletionsController = TextEditingController(
+      text: widget.habit?.totalCompletions.toString() ?? '',
+    );
+    _imageUrlController = TextEditingController(
+      text: widget.habit?.imageUrl ?? '',
+    );
+  }
+
+  @override
+  void dispose() {
+    _habitNameController.dispose();
+    _habitDescriptionController.dispose();
+    _categoryController.dispose();
+    _frequencyController.dispose();
+    _currentStreakController.dispose();
+    _totalCompletionsController.dispose();
+    _imageUrlController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
