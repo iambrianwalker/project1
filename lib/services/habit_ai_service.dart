b8dfaa7e6d1791bf9a267f8d546f6b3f5a5457af
iambrianwalker <bwalker37837@gmail.com>
2026-03-22 12:45:00 -0400

    feat: Added strong streak message and inactive habit mesage for ai

--- lib/services/habit_ai_service.dart
+++ lib/services/habit_ai_service.dart
@@ -7,5 +7,15 @@ class HabitAIService{
     if (analysis.daysSinceLast > 2) {
       return "You've been off track. Let's reset today.";
     }
+
+    //Strong Streak
+    if (analysis.streak >= 7) {
+      return "${analysis.streak}-day streak. You're locked in!";
+    }
+
+    //Inactive Habit
+    if (!analysis.isActive) {
+      return "This habit is fading. Bring it back today!";
+    }
   }
 }
