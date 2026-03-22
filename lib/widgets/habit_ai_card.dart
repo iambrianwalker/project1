476dd94f8404cd5c3c2b2afb0e5820a7aafcd15c
iambrianwalker <bwalker37837@gmail.com>
2026-03-22 12:58:41 -0400

    feat: Started habitAiCard

---
+++ lib/widgets/habit_ai_card.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import '../models/habit_model.dart';
+import '../services/habit_service.dart';
+import '../services/habit_analyzer.dart';
+import '../services/habit_ai_service.dart';
+
+class HabitAICard extends StatefulWidget {
+  final Habit habit;
+  final HabitService habitService;
+
+  const HabitAICard({
+    super.key,
+    required this.habit,
+    required this.habitService,
+  });
+
+  @override
+  
+}
