64140e22527cf219a61f47b85ec053a8bda7ded6
iambrianwalker <bwalker37837@gmail.com>
2026-03-22 14:16:57 -0400

    feat: created all variables for the dashboard functions

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:habit_mastery/models/habit_completion.dart';
 import '../models/habit_model.dart';
 import '../services/habit_service.dart';
 import '../services/habit_ai_service.dart';
@@ -17,4 +18,19 @@ class DashboardScreen extends StatefulWidget{
 
   @override
   State<DashboardScreen> createtate() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  late Future<List> _activeHabitsFuture;
+  late Future<int> _completedTodayFuture;
+  late Future<int> _longestStreakFuture;
+  String? _aiBuddyMessage;
+  List<HabitCompletion> _recentActivity = [];
+  Map<String, bool> _weeklyProgress = {};
+
+  @override
+  void initState(){
+    super.initState();
+    _loadDashboardData();
+  }
 }
